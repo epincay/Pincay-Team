f00cb3a4a7c0519176af3f753ad456b7ff5527a9
epincay <eddiepincay@gmail.com>
2017-02-12 01:22:00 -0500

    Lesson 06

---
+++ escapeMiserie/src/byui/cit260/escapeMiserie/control/ItemControl.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.escapeMiserie.control;
+
+/**
+ *
+ * @author Eddie Pincay
+ */
+public class ItemControl {
+    public double addToInventoryType(double inventoryType, double amount){
+        
+        if(inventoryType < 0 || inventoryType > 9){ //The inventoryType is between 1-8
+            return -1;
+        }
+        if (amount < 0){ //The amount is no possible negative number
+            return -1;
+        }
+        amount ++;
+        return amount;
+    }
+}
