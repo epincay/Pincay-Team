5f6ab6f54f9b8109a3ca872bc836ca7a4ca7fd17
Eddie Pincay <eddiepincay@gmail.com>
2017-03-11 19:06:45 -0500

    Update ItemControl.java

--- escapeMiserie/src/byui/cit260/escapeMiserie/control/ItemControl.java
+++ escapeMiserie/src/byui/cit260/escapeMiserie/control/ItemControl.java
@@ -21,7 +21,7 @@ public class ItemControl {
         amount ++;
         return amount;
     }
-<<<<<<< HEAD
+
     public double removeFromInventory(double inventoryType, double amount){
         if(inventoryType < 0 || inventoryType > 9){ //The inventoryType is between 1-8
             return -1;
@@ -32,6 +32,5 @@ public class ItemControl {
         amount --;
         return amount;
     }
-=======
->>>>>>> origin/master
+
 }
